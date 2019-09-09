35ad8249a6c54528d39bfdbfdee1af3b6cbd39f1
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2019-09-09 01:10:39 -0700

    Added argument parser on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 
-int main( int argc, char *argv[] )
+#include <map>
+
+using ProgramOptions = std::map< std::string, std::string >;
+
+ProgramOptions extract_options( int const argc, char const *argv[] );
+
+int main( int const argc, char const *argv[] )
 {
+    ProgramOptions const options{extract_options( argc, argv )};
+
+    auto it = options.find( "-f" );
+
+    if( it == options.end() )
+    {
+        // cin
+        // Cipher.encode(cin)
+    }
+    else
+    {
+        // file
+        // std::ifstream file....
+        // Cipher.encode(file)
+    }
+
     return 0;
+}
+
+ProgramOptions extract_options( int const argc, char const *argv[] )
+{
+    ProgramOptions options;
+
+    bool flag{true};
+    std::string last_param;
+
+    for( int i{1}; i < argc; ++i )
+    {
+        std::string const param{argv[i]};
+
+        if( flag )
+        {
+            options[param] = "";
+
+            if( param.substr( 0, 2 ) != "--" )
+            {
+                flag = false;
+            }
+        }
+        else
+        {
+            options[last_param] = param;
+
+            flag = true;
+        }
+
+        last_param = param;
+    }
+
+    return options;
 }
